44477df31e2c813f25cb178969728013606d87c7
nhi249007-code <nhi249007@gmail.com>
2026-04-01 17:20:25 +0700

    Update GameFrame.java

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -5,10 +5,10 @@ public class GameFrame extends JFrame {
     public GameFrame(GamePanel panel) {
         this.setTitle("Khong Kich 365");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(panel); // Nạp giao diện vào khung
-        this.pack(); // Tự động co dãn theo kích thước panel
-        this.setSize(800, 600); // Hoặc đặt kích thước cố định
-        this.setLocationRelativeTo(null); // Hiển thị ra giữa màn hình
-        this.setVisible(true); // Có thể để luôn ở đây cho chắc chắn
+        this.add(panel); 
+        this.pack(); 
+        this.setSize(800, 600); 
+        this.setLocationRelativeTo(null); 
+        this.setVisible(true); 
     }
 }
